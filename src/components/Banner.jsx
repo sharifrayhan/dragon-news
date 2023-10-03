edfbd3f45ceccf5beec1a45988816ddce94117ea
sharifrayhan <sharifrayhan.official@gmail.com>
2023-10-03 17:01:55 +0600

    banner section complete

---
+++ src/components/Banner.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import Marquee from "react-fast-marquee";
+
+const Banner = () => {
+  const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    fetch("news_data.json")
+      .then((result) => result.json())
+      .then((data) => setInfo(data));
+  }, [info]);
+ 
+  return (
+    <div className=" flex items-center gap-3 bg-[#F3F3F3] p-4 mt-3">
+      <button className=" bg-[#D72050] font-medium text-white px-4 py-1">Latest</button>
+      <div className=" font-semibold">
+        <Marquee>
+            {
+            
+            info.map((set) => set.subtitle).join(" ** ")
+
+            }
+            </Marquee>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import Header from '../shared/Header';
 import Navbar from '../shared/Navbar';
+import Leftbox from '../components/Leftbox';
+import Middlebox from '../components/Middlebox';
+import Rightbox from '../shared/Rightbox';
+import Banner from '../components/Banner';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Banner></Banner>
             <Navbar></Navbar>
-            This is home
+            <div className=' grid grid-cols-1 md:grid-cols-4 gap-6'>
+                <div>
+                    <Leftbox></Leftbox>
+                </div>
+                <div className=' col-span-2'>
+                    <Middlebox></Middlebox>
+                </div>
+                <div>
+                    <Rightbox></Rightbox>
+                </div>
+            </div>
         </div>
     );
 };
